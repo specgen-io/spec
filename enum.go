ca8d7ae4c4e75c8ab6489cc583d739c1ae7e2b5d
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2019-10-21 23:55:29 -0400

    Added description for enum item

--- enum.go
+++ enum.go
@@ -1,5 +1,86 @@
 package spec
 
+import "gopkg.in/yaml.v2"
+
+type EnumItem struct {
+	Description *string `yaml:"description"`
+}
+
+type NamedEnumItem struct {
+	Name Name
+	EnumItem
+}
+
+type Items []NamedEnumItem
+
+func unmarshalNamesArray(unmarshal func(interface{}) error) (Items, error) {
+	data := make([]string, 0)
+	err := unmarshal(&data)
+	if err != nil {
+		return nil, err
+	}
+	array := make(Items, len(data))
+	for index, key := range data {
+		name := Name{key}
+		err := name.Check(SnakeCase)
+		if err != nil {
+			return nil, err
+		}
+		array[index] = NamedEnumItem{Name: name, EnumItem: EnumItem{Description: nil}}
+	}
+	return array, nil
+}
+
+func unmarshalNamesMap(unmarshal func(interface{}) error) (Items, error) {
+	data := make(map[string]EnumItem)
+	err := unmarshal(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	names := make(yaml.MapSlice, 0)
+	err = unmarshal(&names)
+	if err != nil {
+		return nil, err
+	}
+
+	array := make(Items, len(names))
+	for index, item := range names {
+		key := item.Key.(string)
+		name := Name{key}
+		err := name.Check(SnakeCase)
+		if err != nil {
+			return nil, err
+		}
+		array[index] = NamedEnumItem{Name: name, EnumItem: data[key]}
+	}
+	return array, nil
+}
+
+func isItemsArray(unmarshal func(interface{}) error) bool {
+	data := make([]string, 0)
+	err := unmarshal(&data)
+	return err == nil
+}
+
+func (value *Items) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if isItemsArray(unmarshal) {
+		array, err := unmarshalNamesArray(unmarshal)
+		if err != nil {
+			return err
+		}
+		*value = array
+	} else {
+		array, err := unmarshalNamesMap(unmarshal)
+		if err != nil {
+			return err
+		}
+		*value = array
+	}
+	return nil
+}
+
 type Enum struct {
-	Items []Name `yaml:"enum"`
+	Items       Items   `yaml:"enum"`
+	Description *string `yaml:"description"`
 }
--- enum_test.go
+++ enum_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func Test_Enum_Unmarshal(t *testing.T) {
+func Test_Enum_Short_Unmarshal(t *testing.T) {
 	data := `
 enum:
 - the_first
@@ -17,6 +17,35 @@ enum:
 	var enum = Enum{}
 	err := yaml.UnmarshalStrict([]byte(data), &enum)
 	assert.Equal(t, err, nil)
-	expected := []Name{{"the_first"}, {"the_second"}, {"the_third"}}
+	expected := Items{
+		{Name{"the_first"}, EnumItem{Description: nil}},
+		{Name{"the_second"}, EnumItem{Description: nil}},
+		{Name{"the_third"}, EnumItem{Description: nil}},
+	}
+	assert.Equal(t, reflect.DeepEqual(enum.Items, expected), true)
+}
+
+func Test_Enum_Long_Unmarshal(t *testing.T) {
+	data := `
+enum:
+  the_first:
+    description: First option
+  the_second:
+    description: Second option
+  the_third:
+    description: Third option
+`
+	var enum = Enum{}
+	err := yaml.UnmarshalStrict([]byte(data), &enum)
+	assert.Equal(t, err, nil)
+
+	description1 := "First option"
+	description2 := "Second option"
+	description3 := "Third option"
+	expected := Items{
+		{Name{"the_first"}, EnumItem{Description: &description1}},
+		{Name{"the_second"}, EnumItem{Description: &description2}},
+		{Name{"the_third"}, EnumItem{Description: &description3}},
+	}
 	assert.Equal(t, reflect.DeepEqual(enum.Items, expected), true)
 }

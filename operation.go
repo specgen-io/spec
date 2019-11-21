062a9c9e9007cf8a3705e0169d5d991359fd3aef
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2019-11-21 00:04:50 -0500

    Cleanup YAML helpers

--- operation.go
+++ operation.go
@@ -86,24 +86,26 @@ type Api struct {
 type Apis []Api
 
 func (value *Apis) UnmarshalYAML(node *yaml.Node) error {
-	data := make(map[string]Operations)
-	err := node.Decode(&data)
-	if err != nil {
-		return err
+	if node.Kind != yaml.MappingNode {
+		return errors.New("apis should be YAML mapping")
 	}
-
-	names := mappingKeys(node)
-	array := make([]Api, len(names))
-	for index, key := range names {
-		name := Name{key}
+	count := len(node.Content) / 2
+	array := make([]Api, count)
+	for index := 0; index < count; index++ {
+		keyNode := node.Content[index*2]
+		valueNode := node.Content[index*2+1]
+		name := Name{keyNode.Value}
 		err := name.Check(SnakeCase)
 		if err != nil {
 			return err
 		}
-		dataItem := data[key]
-		array[index] = Api{Name: name, Operations: dataItem}
+		operations := Operations{}
+		err = valueNode.Decode(&operations)
+		if err != nil {
+			return err
+		}
+		array[index] = Api{Name: name, Operations: operations}
 	}
-
 	*value = array
 	return nil
 }
--- yaml.go
+++ yaml.go
@@ -15,14 +15,6 @@ func getMappingKey(mapping *yaml.Node, key string) *yaml.Node {
 	return nil
 }
 
-func mappingKeys(mapping *yaml.Node) []string {
-	keys := make([]string, len(mapping.Content)/2)
-	for i := 0; i < len(mapping.Content)/2; i++ {
-		keys[i] = mapping.Content[i*2].Value
-	}
-	return keys
-}
-
 func getDescription(node *yaml.Node) *string {
 	if node == nil {
 		return nil
